1cf0d21cee57db819c254436976a07d2349d339e
Thomas Boileau <t-boileau@email.com>
2019-02-01 00:40:26 +0100

    Add Symfony bundle class

---
+++ src/TBoileauFormHandlerBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * (c) Thomas Boileau <t-boileau@email.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace TBoileau\FormHandlerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class TBoileauFormHandlerBundle
+ * @package TBoileau\FormHandlerBundle
+ * @author Thomas Boileau <t-boileau@email.com>
+ */
+class TBoileauFormHandlerBundle extends Bundle
+{
+
+}
